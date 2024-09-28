027c8b3ee9814f73d5f6e3fe854acf38574dcc8a
Gamata Sophia Nicole M. <mecossophia@gmail.com>
2024-09-28 23:03:55 +0800

    Add files via upload

---
+++ ageCheck.js
@@ -0,0 +1,12 @@
+let age = 14;
+let EnteryourAge = age
+
+console.log("Enter Your Age:", EnteryourAge);
+if (age < 13){console.log("You are a child.");}
+else if(age <20){console.log("You are a teenager.")}
+else
+{console.log("You are an  adult")
+}
+
+
+
---
+++ gradeAssignment.js
@@ -0,0 +1,8 @@
+let grade = 90;
+let EnteryourScore = grade
+
+console.log("Enter your score", EnteryourScore);
+
+if(grade>=90){console.log("Excellent");}
+else if (grade >= 80){console.log("Good");}
+else{console.log("Needs improvement");}
